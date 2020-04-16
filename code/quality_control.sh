f7c62d48e9a5f3d90b7bb52236d6d0abea180835
Karl-Svard <karl.svard970@gmail.com>
2020-04-16 19:04:30 +0200

    Added the results from the quality control.

--- code/quality_control.sh
+++ code/quality_control.sh
@@ -3,8 +3,8 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:15:00
-#SBATCH -J Quality control
+#SBATCH -t 00:50:00
+#SBATCH -J Quality_control
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user karl.svard970@gmail.com
 
@@ -15,4 +15,12 @@ module load FastQC/0.11.8
 # Commands
 fastqc -f fastq -t 2 \
 /home/karsva/Genome_Analysis/data/raw_data/DNA_trimmed/*.gz \
--o /home/karsva/Genome_Analysis/analyses/02_quality_control/DNA_trimmed_QC 
+-o /home/karsva/Genome_Analysis/analyses/02_quality_control/DNA_trimmed_QC
+
+fastqc -f fastq -t 2 \
+/home/karsva/Genome_Analysis/data/raw_data/RNA_untrimmed/*.gz \
+-o /home/karsva/Genome_Analysis/analyses/02_quality_control/RNA_untrimmed_QC
+
+fastqc -f fastq -t 2 \
+/home/karsva/Genome_Analysis/data/analysis_results/01_preprocessing/RNA_trimmed/*.paired.trimmed.fastq.gz \
+-o /home/karsva/Genome_Analysis/analyses/02_quality_control/RNA_trimmed_QC 
